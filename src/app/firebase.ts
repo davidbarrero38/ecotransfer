bf993db0158dcf3608ff0c651b9d555d9d6d02b4
David L Barrero <davidlopezbarrero@gmail.com>
2019-08-31 14:08:37 +0200

    fire.ts

---
+++ src/app/firebase.ts
@@ -0,0 +1,12 @@
+const Config = {
+    // Put here your firebase configuration
+    apiKey: 'AIzaSyCgW4Meoo2pPRIUps2JT4BpGrIZ5mpEv-Y',
+    authDomain: 'eco-transfer.firebaseapp.com',
+    databaseURL: 'https://eco-transfer.firebaseio.com',
+    projectId: 'eco-transfer',
+    storageBucket: 'eco-transfer.appspot.com',
+    messagingSenderId: '806184084678',
+    appId: '1:806184084678:web:6675426462802bc5'
+};
+
+export default Config;
